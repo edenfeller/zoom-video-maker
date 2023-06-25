c8b38f6a55001f3072d9e46ffaa69b370ee90249
edenfeller <edenfeller@gmail.com>
2023-06-25 19:46:01 +0300

    analyitcs

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ImageToZoomVideo from './zoom2'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ImageToZoomVideo />
+    <Analytics/>
   </React.StrictMode>,
 )
